1e55403efe92de3a8276ca387f0e404f44e9248b
Fredrik H <ique@3h.se>
2022-12-20 18:33:23 +0100

    Numbers weren't unique, I shouldn't have assumed that...

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -2,51 +2,39 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-fn read_input(file_name: &str) -> Vec<i32> {
+fn read_input(file_name: &str) -> Vec<(i64, i64)> {
     let mut numbs = Vec::new();
     let file = File::open(file_name.to_string()).expect("Couldn't open file");
     let reader = BufReader::new(file);
-    for line in reader.lines() {
+    for (i, line) in reader.lines().enumerate() {
         let row = line.expect("Should be able to read line");
-        numbs.push(row.parse::<i32>().expect("Should be parseable"));
+        numbs.push((i as i64, row.parse::<i64>().expect("Should be parseable")));
     }
     numbs
 }
 
-// I assume all numbers are unique
-fn mix(numbers: &Vec<i32>) -> VecDeque<i32> {
-    let mut mixer: VecDeque<i32> = numbers.clone().into();
-    for (_it, &n) in numbers.iter().enumerate() {
-        //println!("n: {}, {:?}", n, mixer);
-        if let Some(i) = mixer.iter().position(|&x| x == n) {
+fn mix(numbers: &Vec<(i64, i64)>, iters: i64) -> VecDeque<(i64, i64)> {
+    let mut mixer: VecDeque<(i64, i64)> = numbers.clone().into();
+    for _ in 0..iters {
+        for (idx, _) in numbers.iter() {
+            let i = mixer.iter().position(|&(k, _)| k == *idx).unwrap();
             mixer.rotate_left(i);
-            let x = mixer.pop_front();
-            let len = mixer.len() as i32;
-            let mut j = n;
-            while j < 0 {
-                j += len;
-            }
-            while j >= len {
-                j -= len;
-            }
-            //println!("{:?}", x);
-            //println!("it: {}, i: {}, n: {}, j: {}, len: {}", _it, i, n, j, len);
+            let x = mixer.pop_front().unwrap();
+            let j = x.1.rem_euclid(mixer.len() as i64);
             mixer.rotate_left(j as usize);
-            mixer.push_back(n);
+            mixer.push_front(x);
         }
     }
-    //println!("{:?}", mixer);
     mixer
 }
 
-fn part1(numbers: &Vec<i32>) -> Option<i32> {
-    let mixed = mix(&numbers);
-    let mut cycle = mixed.iter().cycle();
-    cycle.find(|&&x| x == 0);
+fn get_answer(numbers: &Vec<(i64, i64)>, iters: i64) -> Option<i64> {
+    let mut mixed = mix(&numbers, iters);
+    let z_pos = mixed.iter().position(|&(_, x)| x == 0).unwrap();
+    mixed.rotate_left(z_pos);
     let mut ans = 0;
     for i in vec![1000, 2000, 3000] {
-        let mut c = cycle.clone().skip(i - 1); // -1 because find starts _after_ what it found
-        let next = c.next()?;
+        let (_, next) = mixed[i % mixed.len()];
         println!("{}", next);
         ans += next;
     }
@@ -54,9 +42,15 @@ fn part1(numbers: &Vec<i32>) -> Option<i32> {
 }
 
 fn main() {
-    let numbers = read_input("sample.txt");
-    println!("{:?}", part1(&numbers));
-
+    // Part 1
     let numbers = read_input("input.txt");
-    println!("{:?}", part1(&numbers));
+    println!("{:?}", get_answer(&numbers, 1));
+
+    // Part 2
+    let part2_numbers = numbers
+        .clone()
+        .iter()
+        .map(|(i, v)| (*i, v * 811589153))
+        .collect();
+    println!("{:?}", get_answer(&part2_numbers, 10))
 }
