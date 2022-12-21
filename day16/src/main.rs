4436bf5d86c2ec627ec27ab13b1e5b32447d35c9
Fredrik H <ique@3h.se>
2022-12-21 16:52:20 +0100

    WIP: Making it faster

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,12 +1,13 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 type Graph = HashMap<String, (i32, Vec<String>)>;
+type ValveState = Vec<(String, i32)>;
 
-fn read_input(file_name: String) -> Graph {
+fn read_input(file_name: &str) -> Graph {
     let mut valves = HashMap::new();
-    let file = File::open(file_name).expect("Couldn't open file");
+    let file = File::open(file_name.to_string()).expect("Couldn't open file");
     let reader = BufReader::new(file);
     for line in reader.lines() {
         let row = line.expect("Should be able to read line");
@@ -25,113 +26,185 @@ fn read_input(file_name: String) -> Graph {
     valves
 }
 
-// The condition to go back or not is that the total_flow now must be higher than what it was
-// When I visited the node last time, or we had no benefit from going back to this point
-fn dfs(
-    valves: &Graph,
-    visited: &mut HashMap<String, i32>,
-    opened: &mut HashSet<String>,
-    results: &mut Vec<i32>,
-    minutes_left: i32,
-    total_flow: i32,
-    curr: String,
-) {
-    visited.insert(curr.clone(), total_flow);
-    if valves[&curr].0 == 0 {
-        // Consider this valve opened without wasting time on opening it
-        opened.insert(curr.clone());
-    }
-    if minutes_left <= 0 {
-        return results.push(total_flow);
-    } else if opened.len() == valves.len() {
-        return results.push(total_flow);
-    }
-
-    // Our "moves" from here are to open if it's not opened and restart the iteration on the same place
-    // Or two not open and then move to one of the neighbors
-
-    if !opened.contains(&curr) {
-        let mut new_o = opened.clone();
-        new_o.insert(curr.clone());
-        let new_flow = total_flow + (minutes_left - 1) * valves[&curr].0;
-        // minutes_left - 1 since it takes a minute to open the valve
-        dfs(
-            valves,
-            &mut visited.clone(),
-            &mut new_o.clone(),
-            results,
-            minutes_left - 1, // One minute to open
-            new_flow,
-            curr.clone(),
-        );
-    }
-    for n in valves[&curr].1.iter() {
-        // If we've already been there, but we're now at a higher flow, we can go back
-        // If we haven't been there, we can go there
-        if visited.contains_key(n) && visited[n] < total_flow || !visited.contains_key(n) {
-            dfs(
-                valves,
-                &mut visited.clone(),
-                &mut opened.clone(),
-                results,
-                minutes_left - 1,
-                total_flow,
-                n.to_string(),
-            );
-        }
-    }
-    results.push(total_flow);
+// By default open all the 0 valves because we don't want to waste time trying to open them
+fn initial_valve_state(valves: &Graph) -> ValveState {
+    valves
+        .iter()
+        .filter(|(_, v)| v.0 == 0)
+        .map(|(k, _)| (k.clone(), 0))
+        .collect()
 }
 
-//enum Move {
-//    Open(String),
-//    StepTo(String),
-//}
-//
-//fn better_dfs(valves: &Graph, start: String) {
-//    let mut queue = vec![Move::StepTo(start.clone())];
-//    // Value is (opened, minutes_left, total_flow)
-//    let mut visited: HashMap<String, (bool, i32, i32)> = HashMap::new();
-//    visited.insert(start, (false, 30, 0));
-//
-//    while !queue.is_empty() {
-//        let curr = queue.pop().expect("Queue can't be empty here");
-//        match curr {
-//            Move::Open(n) => (),
-//            Move::StepTo(n) => (),
-//        }
-//        // In addition to the moves to other nodes, we have the option of opening a valve
-//        // if it's visited and not opened or not visited, then open it
-//
-//        if !visited[&curr].0 {
-//            queue.push(Move::Open(curr));
-//            let new_flow = curr_node.2 + valves[&curr].0 * (curr_node.1 - 1);
-//            visited.insert(curr.clone(), (true, curr_node.1 - 1, new_flow));
-//        }
-//        // Add all relevant neighbors to exploration
-//        for n in valves[&curr].1.iter() {
-//            if visited.contains_key(n) && visited[n].2 < visited[&curr].2
-//                || !visited.contains_key(n)
-//            {
-//                queue.push(n.clone());
-//            }
-//        }
-//    }
-//}
+fn better_dfs(valves: &Graph, time: i32) -> i32 {
+    // queue item is Node, Time, Flow, Opened Valves
+    let mut queue = VecDeque::from([("AA".to_string(), 0, 0, initial_valve_state(valves))]);
+    let mut seen: HashSet<(String, ValveState)> = HashSet::new();
+    let mut best = 0;
+
+    while !queue.is_empty() {
+        let (curr_node, minute, flow, valve_state) =
+            queue.pop_front().expect("Queue can't be empty here");
+
+        if minute == time {
+            continue;
+        }
+        if seen.contains(&(curr_node.clone(), valve_state.clone())) {
+            continue;
+        }
+        seen.insert((curr_node.clone(), valve_state.clone()));
+        best = best.max(flow);
+
+        // Nothing more to do if all valves are opened
+        if valve_state.len() == valves.len() {
+            continue;
+        }
+
+        // If we make it here, we haven't been here before in this state.
+        // Check if the valve is opened and open it if not
+        // Otherwise continue to move to each of the possible locations
+
+        if !valve_state.iter().find(|(k, _)| *k == curr_node).is_some() {
+            let mut opened_state = valve_state.clone();
+            opened_state.push((curr_node.clone(), minute + 1));
+            let new_flow = flow + (time - minute - 1) * valves[&curr_node].0;
+            queue.push_back((curr_node.clone(), minute + 1, new_flow, opened_state));
+        }
+
+        for next in valves[&curr_node].1.iter() {
+            queue.push_back((next.clone(), minute + 1, flow, valve_state.clone()));
+        }
+    }
+    return best;
+}
+
+fn both(valves: &Graph, time: i32) -> i32 {
+    // queue item is Node, Time, Flow, Opened Valves
+    let mut queue = VecDeque::from([(
+        "AA".to_string(),
+        "AA".to_string(),
+        0,
+        0,
+        initial_valve_state(valves),
+    )]);
+    let mut seen: HashSet<(String, String, ValveState)> = HashSet::new();
+
+    let mut best = 0;
+    let mut skips = 0;
+
+    while !queue.is_empty() {
+        let (curr_node, ele_node, minute, flow, valve_state) =
+            queue.pop_front().expect("Queue can't be empty here");
+        if seen.len() % 100000 == 0 {
+            println!(
+                "q: {:?}, s: {}, skips: {}, flow: {}, best: {}",
+                queue.len(),
+                seen.len(),
+                skips,
+                flow,
+                best
+            );
+        }
+        if minute == time {
+            continue;
+        }
+        if seen.contains(&(curr_node.clone(), ele_node.clone(), valve_state.clone())) {
+            skips += 1;
+            continue;
+        }
+        seen.insert((curr_node.clone(), ele_node.clone(), valve_state.clone()));
+        best = best.max(flow);
+
+        // Nothing more to do if all valves are opened
+        if valve_state.len() == valves.len() {
+            continue;
+        }
+
+        // If we make it here, we haven't been here before in this state.
+        // Check if the valve is opened and open it if not
+        // Otherwise continue to move to each of the possible locations
+
+        let mut opened_state = valve_state.clone();
+        let mut new_flow = flow;
+        let mut man_opened = false;
+        let mut ele_opened = false;
+
+        if !opened_state.iter().find(|(k, _)| *k == curr_node).is_some() {
+            man_opened = true;
+            opened_state.push((curr_node.clone(), minute + 1));
+            new_flow += (time - minute - 1) * valves[&curr_node].0;
+        }
+        if !opened_state.iter().find(|(k, _)| *k == ele_node).is_some() {
+            ele_opened = true;
+            opened_state.push((ele_node.clone(), minute + 1));
+            new_flow += (time - minute - 1) * valves[&ele_node].0;
+        }
+
+        if ele_opened && !man_opened {
+            // Ele stands still but man can move
+            for next in valves[&curr_node].1.iter() {
+                if !seen.contains(&(next.clone(), ele_node.clone(), opened_state.clone())) {
+                    queue.push_back((
+                        next.clone(),
+                        ele_node.clone(),
+                        minute + 1,
+                        new_flow,
+                        opened_state.clone(),
+                    ));
+                }
+            }
+        }
+        if man_opened && !ele_opened {
+            // Man stands still but ele can move
+            for next in valves[&ele_node].1.iter() {
+                if !seen.contains(&(curr_node.clone(), next.clone(), opened_state.clone())) {
+                    queue.push_back((
+                        curr_node.clone(),
+                        next.clone(),
+                        minute + 1,
+                        new_flow,
+                        opened_state.clone(),
+                    ));
+                }
+            }
+        }
+        if man_opened && ele_opened {
+            if !seen.contains(&(curr_node.clone(), ele_node.clone(), opened_state.clone())) {
+                queue.push_back((
+                    curr_node.clone(),
+                    ele_node.clone(),
+                    minute + 1,
+                    new_flow,
+                    opened_state.clone(),
+                ));
+            }
+        }
+
+        for next_man in valves[&curr_node].1.iter() {
+            for next_ele in valves[&ele_node].1.iter() {
+                if !seen.contains(&(next_man.clone(), next_ele.clone(), valve_state.clone())) {
+                    queue.push_back((
+                        next_man.clone(),
+                        next_ele.clone(),
+                        minute + 1,
+                        flow,
+                        valve_state.clone(),
+                    ));
+                }
+            }
+        }
+    }
+    println!("best: {}", best);
+    return best;
+}
 
 fn main() {
-    let mut visited: HashMap<String, i32> = HashMap::new();
-    let mut opened: HashSet<String> = HashSet::new();
-    let mut results: Vec<i32> = Vec::new();
-    let valves = read_input("input.txt".to_string());
-    dfs(
-        &valves,
-        &mut visited,
-        &mut opened,
-        &mut results,
-        30,
-        0,
-        "AA".to_string(),
-    );
-    println!("{:?}", results.iter().max());
+    let valves = read_input("input.txt");
+
+    // Part 1
+    //let ans = better_dfs(&valves, 30);
+    //println!("{:?}", ans);
+
+    // Part 2
+    let ans2 = both(&valves, 26);
+    println!("{:?}", ans2);
 }
