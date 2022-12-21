89ed77322b7459e70ced334060f38f1b8c915fc2
Fredrik H <ique@3h.se>
2022-12-21 21:29:08 +0100

    WIP

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -38,24 +38,41 @@ fn initial_valve_state(valves: &Graph) -> ValveState {
 fn better_dfs(valves: &Graph, time: i32) -> i32 {
     // queue item is Node, Time, Flow, Opened Valves
     let mut queue = VecDeque::from([("AA".to_string(), 0, 0, initial_valve_state(valves))]);
-    let mut seen: HashSet<(String, ValveState)> = HashSet::new();
+    let mut seen: HashSet<(String, i32, ValveState)> = HashSet::new();
     let mut best = 0;
+
+    let mut hit_time = 0;
+    let mut seen_cnt = 0;
+    let mut all_op = 0;
 
     while !queue.is_empty() {
         let (curr_node, minute, flow, valve_state) =
-            queue.pop_front().expect("Queue can't be empty here");
+            queue.pop_back().expect("Queue can't be empty here");
 
+        println!(
+            "q: {:?}, s: {}, time: {}, seen: {}, opened: {}, all_op: {}, best: {}",
+            queue.len(),
+            seen.len(),
+            hit_time,
+            seen_cnt,
+            all_op,
+            flow,
+            best
+        );
         if minute == time {
+            hit_time += 1;
             continue;
         }
-        if seen.contains(&(curr_node.clone(), valve_state.clone())) {
+        if seen.contains(&(curr_node.clone(), minute, valve_state.clone())) {
+            seen_cnt += 1;
             continue;
         }
-        seen.insert((curr_node.clone(), valve_state.clone()));
+        seen.insert((curr_node.clone(), minute, valve_state.clone()));
         best = best.max(flow);
 
         // Nothing more to do if all valves are opened
         if valve_state.len() == valves.len() {
+            all_op += 1;
             continue;
         }
 
@@ -77,6 +94,22 @@ fn better_dfs(valves: &Graph, time: i32) -> i32 {
     return best;
 }
 
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct SeenState {
+    curr: String,
+    ele: String,
+    min: i32,
+    vs: ValveState,
+}
+
+// Alt strategy
+
+// Build a matrix of distances from every node to every other node
+// Go to the valve with the highest immediate benefit taking the distance to it
+// into account and open that one, then from that point repeat until all opened
+// That won't really work.. it's more beneficial to open some valve on the way
+// to some higher value one rather than go directly to high value and then back
+// to the low-value
 fn both(valves: &Graph, time: i32) -> i32 {
     // queue item is Node, Time, Flow, Opened Valves
     let mut queue = VecDeque::from([(
@@ -86,14 +119,21 @@ fn both(valves: &Graph, time: i32) -> i32 {
         0,
         initial_valve_state(valves),
     )]);
-    let mut seen: HashSet<(String, String, ValveState)> = HashSet::new();
+    let mut seen: HashSet<SeenState> = HashSet::new();
 
     let mut best = 0;
     let mut skips = 0;
 
     while !queue.is_empty() {
-        let (curr_node, ele_node, minute, flow, valve_state) =
-            queue.pop_front().expect("Queue can't be empty here");
+        let q = queue.pop_back().expect("Queue can't be empty here");
+        let mut seen_state = SeenState {
+            curr: q.0,
+            ele: q.1,
+            min: q.2,
+            vs: q.4,
+        };
+        let minute = q.2;
+        let flow = q.3;
         if seen.len() % 100000 == 0 {
             println!(
                 "q: {:?}, s: {}, skips: {}, flow: {}, best: {}",
@@ -107,15 +147,15 @@ fn both(valves: &Graph, time: i32) -> i32 {
         if minute == time {
             continue;
         }
-        if seen.contains(&(curr_node.clone(), ele_node.clone(), valve_state.clone())) {
+        if seen.contains(&seen_state) {
             skips += 1;
             continue;
         }
-        seen.insert((curr_node.clone(), ele_node.clone(), valve_state.clone()));
+        seen.insert(seen_state.clone());
         best = best.max(flow);
 
         // Nothing more to do if all valves are opened
-        if valve_state.len() == valves.len() {
+        if seen_state.vs.len() == valves.len() {
             continue;
         }
 
@@ -123,71 +163,81 @@ fn both(valves: &Graph, time: i32) -> i32 {
         // Check if the valve is opened and open it if not
         // Otherwise continue to move to each of the possible locations
 
-        let mut opened_state = valve_state.clone();
+        let mut opened_state = seen_state.vs.clone();
         let mut new_flow = flow;
         let mut man_opened = false;
         let mut ele_opened = false;
 
-        if !opened_state.iter().find(|(k, _)| *k == curr_node).is_some() {
+        if !opened_state
+            .iter()
+            .find(|(k, _)| *k == seen_state.curr)
+            .is_some()
+        {
             man_opened = true;
-            opened_state.push((curr_node.clone(), minute + 1));
-            new_flow += (time - minute - 1) * valves[&curr_node].0;
+            opened_state.push((seen_state.curr.clone(), minute + 1));
+            new_flow += (time - minute - 1) * valves[&seen_state.curr].0;
         }
-        if !opened_state.iter().find(|(k, _)| *k == ele_node).is_some() {
+        if !opened_state
+            .iter()
+            .find(|(k, _)| *k == seen_state.ele)
+            .is_some()
+        {
             ele_opened = true;
-            opened_state.push((ele_node.clone(), minute + 1));
-            new_flow += (time - minute - 1) * valves[&ele_node].0;
+            opened_state.push((seen_state.ele.clone(), minute + 1));
+            new_flow += (time - minute - 1) * valves[&seen_state.ele].0;
         }
 
         if ele_opened && !man_opened {
             // Ele stands still but man can move
-            for next in valves[&curr_node].1.iter() {
-                if !seen.contains(&(next.clone(), ele_node.clone(), opened_state.clone())) {
-                    queue.push_back((
-                        next.clone(),
-                        ele_node.clone(),
-                        minute + 1,
-                        new_flow,
-                        opened_state.clone(),
-                    ));
+            for next in valves[&seen_state.curr].1.iter() {
+                let st = SeenState {
+                    curr: next.to_string(),
+                    ele: seen_state.ele.clone(),
+                    min: seen_state.min.clone(),
+                    vs: opened_state.clone(),
+                };
+                if !seen.contains(&st) {
+                    queue.push_back((st.curr, st.ele, minute + 1, new_flow, st.vs));
                 }
             }
         }
         if man_opened && !ele_opened {
             // Man stands still but ele can move
-            for next in valves[&ele_node].1.iter() {
-                if !seen.contains(&(curr_node.clone(), next.clone(), opened_state.clone())) {
-                    queue.push_back((
-                        curr_node.clone(),
-                        next.clone(),
-                        minute + 1,
-                        new_flow,
-                        opened_state.clone(),
-                    ));
+            for next in valves[&seen_state.curr].1.iter() {
+                let st = SeenState {
+                    curr: seen_state.curr.clone(),
+                    ele: next.to_string(),
+                    min: seen_state.min.clone(),
+                    vs: opened_state.clone(),
+                };
+                if !seen.contains(&st) {
+                    queue.push_back((st.curr, st.ele, minute + 1, new_flow, st.vs));
                 }
             }
         }
         if man_opened && ele_opened {
-            if !seen.contains(&(curr_node.clone(), ele_node.clone(), opened_state.clone())) {
-                queue.push_back((
-                    curr_node.clone(),
-                    ele_node.clone(),
-                    minute + 1,
-                    new_flow,
-                    opened_state.clone(),
-                ));
+            let st = SeenState {
+                curr: seen_state.curr.clone(),
+                ele: seen_state.ele.clone(),
+                min: seen_state.min.clone(),
+                vs: opened_state.clone(),
+            };
+            if !seen.contains(&st) {
+                queue.push_back((st.curr, st.ele, minute + 1, new_flow, st.vs));
             }
         }
 
-        for next_man in valves[&curr_node].1.iter() {
-            for next_ele in valves[&ele_node].1.iter() {
-                if !seen.contains(&(next_man.clone(), next_ele.clone(), valve_state.clone())) {
+        for next_man in valves[&seen_state.curr].1.iter() {
+            for next_ele in valves[&seen_state.ele].1.iter() {
+                seen_state.curr = next_man.to_string();
+                seen_state.ele = next_ele.to_string();
+                if !seen.contains(&seen_state) {
                     queue.push_back((
                         next_man.clone(),
                         next_ele.clone(),
                         minute + 1,
                         flow,
-                        valve_state.clone(),
+                        seen_state.vs.clone(),
                     ));
                 }
             }
